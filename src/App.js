3b0d57df80881b18b4acc9082824d617bdbe9368
masa-bot1 <bdyht515@yahoo.co.jp>
2022-02-06 20:01:25 +0900

    complete components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-// Headerコンポーネントをimportする
 import { Header } from './components/Header';
+import { Skills } from './components/Skills';
+import { Contact } from './components/Contact';
 
 function App() {
   return (
-    <Header /> // Headerコンポーネントを呼び出す
+    <>
+      <Header />
+      <Skills />
+      <Contact />
+    </>
   );
 }
 
