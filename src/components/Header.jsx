e02d6a381fd5d8f18bb0d8436b4a3c6a2e9760c1
masa-bot1 <bdyht515@yahoo.co.jp>
2022-02-06 19:03:22 +0900

    apply style to jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,30 @@
-export const Header = () => {
-  const componentName = () => 'Header';
+// 画像をそれぞれimportします。パスに注意してください。
+import CoverImage from '../images/cover-image.jpg';
+import ProfileImage from '../images/profile-image.png';
 
+export const Header = () => {
   return (
-    <div className="test-wrapper">
-      このコンポーネントの名前は{componentName()}です。
-    </div>
+    <header className="main-cover" style={{backgroundImage: `url(${CoverImage})`}}>
+      {/* overlayはカバー画像の上に透過して表示される背景要素です */}
+      <div className="overlay"></div>
+      <div className="container">
+        <div className="display-table">
+          <div className="display-table-contents">
+            {/* カバー画像 */}
+            <div className="profile-thumb" style={{backgroundImage: `url(${ProfileImage})`}}></div>
+            <h1 className="title-text">m-boy</h1>
+            <h3 className="title-text">Engineer</h3>
+            <ul className="social-icons">
+              <li className="icon-link">
+                {/* TODO */}
+              </li>
+              <li className="icon-link">
+                {/* TODO */}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
